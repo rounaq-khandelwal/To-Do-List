7adcdb270af5fa67134fabbd6f9568328d352127
Rounaq Khandelwal <rounaqkhandelwal24@gmail.com>
2023-09-30 01:03:41 +0530

    Complete the project using React

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,31 @@
+// import React, { useState } from "react";
+// function ToDoItem(props) {
+//   const [isPressed, setIsPressed] = useState(false);
+//   function handleClick(event) {
+//     setIsPressed(!isPressed);
+//   }
+//   return (
+//     <li
+//       onClick={handleClick}
+//       style={{ textDecoration: isPressed ? "line-through" : "none" }}
+//     >
+//       {props.item}
+//     </li>
+//   );
+// }
+// export default ToDoItem;
+
+//------------------------------------------------------------------//
+import React from "react";
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.item}</li>
+    </div>
+  );
+}
+export default ToDoItem;
